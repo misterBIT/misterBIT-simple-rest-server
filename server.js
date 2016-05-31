6a8ecc104cd03d3183b43e4e20c77a1d66e06719
nadavsinai <ns@nadavsinai.com>
2016-05-31 23:17:15 +0300

    add missing cors, make it work for new collections (not in json folder) fix cors to work

--- server.js
+++ server.js
@@ -1,72 +1,76 @@
 // Minimal Simple REST API Handler (in memory)
 // Author: Yaron Biton misterBIT.co.il
 
-var express = require('express'),
+"use strict";
+const express = require('express'),
 	cors = require('cors'),
-  	http = require('http'),
-  	utils = require('./lib/utils.js');
+	utils = require('./lib/utils.js');
 
 
 // Main Cache object, entities are lazily loaded and saved here for in memory CRUD
-var cache = {};
+const cache = {};
 function getObjList(objType) {
 	if (!cache[objType]) {
-		cache[objType] = require('./data/'+objType+'/list.json');
+		try {
+			cache[objType] = require('./data/' + objType + '/list.json');
+		} catch (e) {
+			//case initial file is undefined
+			cache[objType] = [];
+		}
 	}
 	return cache[objType];
 }
 
-var app = express()
+const app = express();
 app.use(cors());
-app.use(express.bodyParser())
+app.use(express.bodyParser());
 
 
-
-app.all('/', function(req, res, next) {
+app.all('/', function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 });
 
-app.get('/data/:objType', function  (req, res) {
-	var objs = getObjList(req.params.objType);
+app.get('/data/:objType', function (req, res) {
+	const objs = getObjList(req.params.objType);
 	cl("Returning list of " + objs.length + " " + req.params.objType + "s");
 	res.json(objs);
 });
 
-app.get('/data/:objType/:id', function  (req, res) {
+app.get('/data/:objType/:id', function (req, res) {
 	cl("GET for single " + req.params.objType);
-	var objs = getObjList(req.params.objType);
-	var index = findIndexForId(objs, req.params.id);
+	const objs = getObjList(req.params.objType);
+	const index = findIndexForId(objs, req.params.id);
 	res.json(objs[index]);
 });
 
-app.put('/data/:objType/:id', function  (req, res) {
+app.put('/data/:objType/:id', function (req, res) {
 	cl("PUT for " + req.params.objType);
-	var objs = getObjList(req.params.objType);
-	var obj = req.body;
+	const objs = getObjList(req.params.objType);
+	const obj = req.body;
 	obj.id = parseInt(req.params.id);
-	var success = updateObj(objs, obj);
+	const success = updateObj(objs, obj);
 	if (success) res.json(obj);
 	else res.json(404, {error: 'not found'})
 });
 
-app.post('/data/:objType', function  (req, res) {
+app.post('/data/:objType', function (req, res) {
 	cl("POST for " + req.params.objType);
-	var objs = getObjList(req.params.objType);
-	var obj = req.body;
+	const objs = getObjList(req.params.objType);
+	const obj = req.body;
 	obj.id = findNextId(objs);
 	addObj(objs, obj);
 	res.json(obj);
 });
 
-app.delete('/data/:objType/:id', function  (req, res) {
-	var objs = getObjList(req.params.objType);
+app.delete('/data/:objType/:id', function (req, res) {
+	const objs = getObjList(req.params.objType);
 	deleteObj(objs, req.params.id);
-    res.json({});
+	res.json({});
 });
 
-http.createServer(app).listen(3003, function () {
-  console.log("misterREST server is ready at http://localhost:3003");
+app.listen(3003, function () {
+	console.log("misterREST server is ready at http://localhost:3003");
 });
 
